2e8ce8504e47870c991d09c20cc1241bff9c57b7
StephenSanjaya <stephensanjaya5@gmail.com>
2024-03-13 16:56:31 +0700

    docs: add DDL for the database

---
+++ config/sport-commerce.sql
@@ -0,0 +1,48 @@
+CREATE TYPE valid_roles AS ENUM ('Customer', 'Admin');
+-- Users Table
+CREATE TABLE Users (
+    UserID SERIAL PRIMARY KEY,
+    Username VARCHAR(100) UNIQUE NOT NULL,
+    Password VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    Address VARCHAR(255),
+    Balance DECIMAL(10, 2),
+    Role valid_roles
+);
+
+-- Categories Table
+CREATE TABLE Categories (
+    CategoryID SERIAL PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL
+);
+
+-- Products Table
+CREATE TABLE Products (
+    ProductID SERIAL PRIMARY KEY,
+    CategoryID INT NOT NULL REFERENCES Categories(CategoryID),
+    Name VARCHAR(100) NOT NULL,
+    Description TEXT,
+    Price DECIMAL(10, 2) NOT NULL,
+    QuantityInStock INT NOT NULL
+);
+
+-- Carts Table
+CREATE TABLE Carts (
+    CartID SERIAL PRIMARY KEY,
+    UserID INT NOT NULL REFERENCES Users(UserID),
+    ProductID INT NOT NULL REFERENCES Products(ProductID),
+    Quantity INT NOT NULL,
+    SubTotal DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TYPE valid_order_status AS ENUM ('Pending', 'Completed', 'Cancelled');
+CREATE TYPE valid_payment_method AS ENUM ('Cash', 'Debit Card', 'Credit Card');
+-- Orders Table
+CREATE TABLE Orders (
+    OrderID SERIAL PRIMARY KEY,
+    UserID INT NOT NULL REFERENCES Users(UserID),
+    OrderDate DATE NOT NULL,
+    TotalAmount DECIMAL(10, 2) NOT NULL,
+    Status valid_order_status NOT NULL DEFAULT 'Pending',
+    PaymentMethod valid_payment_method NOT NULL
+);
