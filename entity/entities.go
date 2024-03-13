b271bf2867743816aa4a03e19c8104470757e114
Azillah Zulhi <azillahzulhi2603@gmail.com>
2024-03-13 17:02:30 +0700

    update entity and handler

---
+++ entity/entities.go
@@ -0,0 +1,6 @@
+package entity
+
+type Users struct {
+	Username, Password, Email, Address, Role string
+	Balance                                  float64
+}
--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,82 @@
 package handler
+
+import (
+	"Sport-PairProject/entity"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var users = make(map[string]entity.Users)
+
+func ShowForm() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("Menu:")
+		fmt.Println("1. Register")
+		fmt.Println("2. Login")
+		fmt.Println("3. Exit")
+
+		fmt.Print("Choose an option: ")
+		optionStr, _ := reader.ReadString('\n')
+		option := strings.TrimSpace(optionStr)
+
+		switch option {
+		case "1":
+			Register(reader)
+		case "2":
+			Login(reader)
+		case "3":
+			fmt.Println("Exiting program.")
+			return
+		default:
+			fmt.Println("Invalid option. Please choose again.")
+		}
+	}
+}
+
+func Register(reader *bufio.Reader) {
+	fmt.Println("Registration")
+	fmt.Print("Enter username: ")
+	username, _ := reader.ReadString('\n')
+	username = strings.TrimSpace(username)
+
+	if _, exists := users[username]; exists {
+		fmt.Println("Username already exists.")
+		return
+	}
+
+	fmt.Print("Enter password: ")
+	password, _ := reader.ReadString('\n')
+	password = strings.TrimSpace(password)
+
+	newUser := entity.Users{
+		Username: username,
+		Password: password,
+	}
+
+	users[username] = newUser
+	fmt.Println("Registration successful.")
+}
+
+func Login(reader *bufio.Reader) {
+	fmt.Println("Login")
+	fmt.Print("Enter username: ")
+	username, _ := reader.ReadString('\n')
+	username = strings.TrimSpace(username)
+
+	fmt.Print("Enter password: ")
+	password, _ := reader.ReadString('\n')
+	password = strings.TrimSpace(password)
+
+	if storedUser, exists := users[username]; exists {
+		if storedUser.Password == password {
+			fmt.Println("Login successful.")
+			return
+		}
+	}
+
+	fmt.Println("Invalid username or password.")
+}
