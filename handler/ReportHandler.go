d46a2b2c8487ba1a7efbab47c404d1b154e5f94b
StephenSanjaya <stephensanjaya5@gmail.com>
2024-03-14 19:25:51 +0700

    feat: add function to generate user report

---
+++ handler/ReportHandler.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"Sport-PairProject/entity"
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+func GetUserReport(db *sql.DB) (user_report []entity.UserReport, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var ur entity.UserReport
+
+	query := "SELECT UserID, Username, Email, Address, Balance FROM Users"
+
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		fmt.Println(err.Error())
+		return []entity.UserReport{}, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(&ur.UserID, &ur.Username, &ur.Email, &ur.Address, &ur.Balance)
+		if err != nil {
+			fmt.Println(err.Error())
+			return []entity.UserReport{}, err
+		}
+		user_report = append(user_report, p)
+	}
+
+	return user_report, nil
+}
