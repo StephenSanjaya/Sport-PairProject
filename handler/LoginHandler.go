d6701f34e64c9e27bdd35a68ce0418c1e072ce98
Azillah Zulhi <azillahzulhi2603@gmail.com>
2024-03-14 22:28:38 +0700

    update register and login

--- handler/LoginHandler.go
+++ handler/LoginHandler.go
@@ -3,10 +3,25 @@ package handler
 import (
 	"Sport-PairProject/entity"
 	"database/sql"
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func LoginUser(db *sql.DB, newUser entity.User) (role string, user_id int) {
 	//login logic
+
+	var email, password string
+	err := db.QueryRow("SELECT UserID, Role, Email, Password FORM Users WHERE Email = ?", newUser.Email).Scan(&user_id, &role, &email, &password)
+	if err != nil {
+		fmt.Println("User not found: ", err)
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(newUser.Password), []byte(password))
+	if err != nil { // for compare input password with hashed password in database with CompareHashAndPassword method
+		return
+	}
 
 	return role, user_id
 }
--- handler/RegisterHandler.go
+++ handler/RegisterHandler.go
@@ -3,8 +3,30 @@ package handler
 import (
 	"Sport-PairProject/entity"
 	"database/sql"
+	"fmt"
+	"regexp"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
-func RegisterUser(db *sql.DB, newUser entity.User) {
-	//register logic
+func RegisterUser(db *sql.DB, newUser entity.User) error {
+	// Check email format
+	emailRegex := regexp.MustCompile("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")
+	if !emailRegex.MatchString(newUser.Email) {
+		return fmt.Errorf("invalid email format")
+	}
+
+	// Hash user password with GenerateFormPassword method
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	// Insert user data into the database
+	_, err = db.Exec("INSERT INTO Users(Username, Email, Password, Address) VALUES (?, ?, ?, ?)", newUser.Username, newUser.Email, hashedPassword, newUser.Address)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
