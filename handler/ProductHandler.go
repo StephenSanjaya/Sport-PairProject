e4dd358b0181347ed7ef99930a15c076b4223c81
StephenSanjaya <stephensanjaya5@gmail.com>
2024-03-15 00:50:43 +0700

    refactor: rename parameter query

--- handler/ProductHandler.go
+++ handler/ProductHandler.go
@@ -12,7 +12,7 @@ func InsertNewProduct(db *sql.DB, product entity.Product) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	query := "INSERT INTO Products (CategoryID, Name, Description, Price, QuantityInStock) VALUES (?,?,?,?)"
+	query := "INSERT INTO Products (CategoryID, Name, Description, Price, QuantityInStock) VALUES ($1,$2,$3,$4,$5)"
 
 	_, err := db.ExecContext(ctx, query, product.CategoryID, product.ProductName, product.Description, product.Price, product.QuantityInStock)
 	if err != nil {
@@ -27,7 +27,7 @@ func GetCurrentStock(db *sql.DB, product_id int) (current_stock int) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	query := "SELECT QuantityInStock FROM Products WHERE ProductID = ?"
+	query := "SELECT QuantityInStock FROM Products WHERE ProductID = $1"
 
 	err := db.QueryRowContext(ctx, query, product_id).Scan(&current_stock)
 	if err != nil {
@@ -44,9 +44,9 @@ func UpdateProductStock(db *sql.DB, stock, product_id int) error {
 
 	newStock := GetCurrentStock(db, product_id) + stock
 
-	query := "UPDATE Products WHERE QuantityInStock = ?"
+	query := "UPDATE Products SET QuantityInStock = $1 WHERE ProductID = $2"
 
-	_, err := db.ExecContext(ctx, query, newStock)
+	_, err := db.ExecContext(ctx, query, newStock, product_id)
 	if err != nil {
 		fmt.Println(err.Error())
 		return err
@@ -55,19 +55,24 @@ func UpdateProductStock(db *sql.DB, stock, product_id int) error {
 	return nil
 }
 
-func DeleteProduct(db *sql.DB, product_id int) error {
+func DeleteProduct(db *sql.DB, product_id int) bool {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	query := "DELETE Products WHERE ProductID = ?"
+	query := "DELETE FROM Products WHERE ProductID = $1"
 
-	_, err := db.ExecContext(ctx, query, product_id)
+	res, err := db.ExecContext(ctx, query, product_id)
 	if err != nil {
 		fmt.Println(err.Error())
-		return err
+		return false
 	}
 
-	return nil
+	count, _ := res.RowsAffected()
+	if count == 0 {
+		return false
+	}
+
+	return true
 }
 
 func GetAllProductList(db *sql.DB) (products []entity.Product, err error) {
@@ -76,7 +81,7 @@ func GetAllProductList(db *sql.DB) (products []entity.Product, err error) {
 
 	var p entity.Product
 
-	query := "SELECT p.ProductID, c.Name, p.Name, p.Description, p.Price, p.QuantityInStock FROM Products p JOIN Categoryies c ON p.CategoryID = c.CategoryID"
+	query := "SELECT p.ProductID, c.Name, p.Name, p.Description, p.Price, p.QuantityInStock FROM Products p JOIN Categories c ON p.CategoryID = c.CategoryID"
 
 	rows, err := db.QueryContext(ctx, query)
 	if err != nil {
